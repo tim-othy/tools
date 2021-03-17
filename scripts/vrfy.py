27d965a37528b53e18ac5c74ef9ef024a8b6ebf2
Timothy Mehay <timothymehay@gmail.com>
2021-03-17 20:44:36 +0100

    Fix vrfy.py

--- scripts/vrfy.py
+++ scripts/vrfy.py
@@ -3,18 +3,20 @@
 import socket
 import sys
 
+
 if len(sys.argv) != 3:
     print("Usage: vrfy.py <ip> <username>")
     sys.exit(0)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connect = s.connect((sys.argv[1], 25))
 banner = s.recv(1024)
 
-print(bannder)
-
-s.send(f"VRFY ${sys.argv[2]}\r\n")
+print(banner)
+s.send("VRFY " + sys.argv[2] + "\r\n")
 result = s.recv(1024)
 
 print(result)
-s.close()
+s.close(0)
+
